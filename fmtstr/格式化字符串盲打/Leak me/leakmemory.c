e972de868e87a315c569902994c3fca63bdd7242
skyedai910 <liangliheng231@gmail.com>
2020-07-14 14:04:16 +0800

    Update documentation

--- fmtstr/格式化字符串盲打/Leak me/leakmemory.c
+++ fmtstr/格式化字符串盲打/Leak me/leakmemory.c
@@ -1,4 +1,6 @@
-#include <stdio.h> #include <string.h> #include <unistd.h> 
+#include <stdio.h> 
+#include <string.h> 
+#include <unistd.h> 
 int main(int argc, char *argv[])
 {
     setbuf(stdin, 0LL);
@@ -7,30 +9,29 @@ int main(int argc, char *argv[])
     int flag;
     char buf[1024];
     FILE* f;
-
     puts("What's your name?");
-
     fgets(buf, 1024, stdin);
     printf("Hi, ");
     printf("%s",buf);
     putchar('\n');
-
     flag = 1;
-    while (flag == 1){
+    while (flag == 1)
+    {
         puts("Do you want the flag?");
-
-	memset(buf,'\0',1024);
-	read(STDIN_FILENO, buf, 100);
-        if (!strcmp(buf, "no\n")){
+        memset(buf,'\0',1024);
+        read(STDIN_FILENO, buf, 100);
+        if (!strcmp(buf, "no\n"))
+        {
             printf("I see. Good bye.");
             return 0;
-        }else
-	{   
-	    printf("Your input isn't right:");
-	    printf(buf);
-	    printf("Please Try again!\n");
-	}
-	fflush(stdout);
+        }
+        else
+        {   
+            printf("Your input isn't right:");
+            printf(buf);
+            printf("Please Try again!\n");
+        }
+        fflush(stdout);
     }
     return 0;
 }
